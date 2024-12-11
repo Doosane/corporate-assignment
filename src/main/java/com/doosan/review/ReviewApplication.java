73f3b51d49ef6c6acb6144e9db08db640eb8dc9e
baekdoosan <doosan0425@gmail.com>
2024-12-11 14:25:48 +0900

    메인 어플리케이션 셋업 , 테스트 커밋

---
+++ src/main/java/com/doosan/review/ReviewApplication.java
@@ -0,0 +1,14 @@
+package com.doosan.review;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@EnableJpaAuditing
+@SpringBootApplication
+public class ReviewApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ReviewApplication.class, args);
+    }
+
+}
